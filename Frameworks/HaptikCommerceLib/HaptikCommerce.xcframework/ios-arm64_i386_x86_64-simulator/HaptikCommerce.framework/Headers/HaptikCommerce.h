e0418ede733ae266660f7fb27dbccba1711c737a
Ketan Doiphode <ketan@haptik.ai>
2020-11-11 17:36:50 +0530

    Add Example project

---
+++ Frameworks/HaptikCommerceLib/HaptikCommerce.xcframework/ios-arm64_i386_x86_64-simulator/HaptikCommerce.framework/Headers/HaptikCommerce.h
@@ -0,0 +1,18 @@
+//
+//  HaptikCommerce.h
+//  HaptikCommerce
+//
+//  Created by Ketan on 28/09/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HaptikCommerce.
+FOUNDATION_EXPORT double HaptikCommerceVersionNumber;
+
+//! Project version string for HaptikCommerce.
+FOUNDATION_EXPORT const unsigned char HaptikCommerceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HaptikCommerce/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# Haptik Commerce
+
+The Buzzo SDK enables brands to turn their app into a Shopping and Ordering Assistant. It enables users to search, filter and sort products by voice, get personalised recommendations and buying guidance,  ask product specific questions as well as to discover reviews based product insights.
+For more information visit www.buzzo.ai
